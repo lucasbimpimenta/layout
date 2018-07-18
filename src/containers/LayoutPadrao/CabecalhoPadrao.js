cf809514f1b73ce6fa873ba5eab4ae79f7139833
Lucas Benjamim Botelho Silva Pimenta <c096810@corp.caixa.gov.br>
2018-07-18 13:55:19 -0300

    CSS

--- src/containers/LayoutPadrao/CabecalhoPadrao.js
+++ src/containers/LayoutPadrao/CabecalhoPadrao.js
@@ -14,9 +14,20 @@ class CabecalhoPadrao extends Component {
             <div class="collapse navbar-collapse" id="navbarBootstrapTiNegocios">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
-                        <a class="nav-link cx-style-setter" href="" cx-style="cosmo">Cosmo
+                        <a class="nav-link cx-style-setter" href="" cx-style="cosmo">Início
                             <span class="sr-only">(current)</span>
                         </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
                     </li>
                 </ul>
                 <ul class="navbar-nav navbar-right">
